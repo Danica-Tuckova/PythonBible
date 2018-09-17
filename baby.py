3dbb63ef5c1a64a28b015306afb836892bd24619
unknown <danicatuckova@gmail.com>
2018-09-17 22:46:29 +0200

    stimulator

---
+++ baby.py
@@ -0,0 +1,15 @@
+# make a list of questions 
+# pick a random one question from the list for the baby to use
+
+# z balika random vybrat funkciu choice
+from random import choice
+questions = ["Why is the sky blue?", "Why is there a face on the moon?", "Where are all the dinosaurus?"]
+
+question = choice(questions)
+
+# baby`s question and get the answear
+answer = input(question).strip().lower()
+
+while answer != "just because":
+    answer = input("Why? ").strip().lower()
+print("Oh...Okay")    

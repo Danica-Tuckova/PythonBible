b1fb618bd3fc824606c1ee57378d51587beda8a1
unknown <danicatuckova@gmail.com>
2018-09-19 19:26:49 +0200

    for loop

---
+++ list_comprehesions.py
@@ -0,0 +1,8 @@
+# go through all the numbers between 1 and 100 and create a list of even numbers
+
+even_numbers = [x for x in range(1, 101) if x % 2 == 0]
+print(even_numbers)
+
+# go through all the numbers between 1 and 100 and create a list of odd numbers
+odd_numbers = [y for y in range(1,101) if y % 3 == 0]
+print(odd_numbers)
